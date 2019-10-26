e3394ccc3db92bd6a1260acc042930ad3b21a19a
Kira <kira.mueller@outlook.com>
2019-10-26 11:31:48 -0700

    initial game instantiation on page load

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Backdrop from './components/Backdrop';
 import GameContainer from './components/GameContainer';
 import PreviewBlocks from './components/PreviewBlocks';
 import Score from './components/Score';
+import Engine from 'tetris-engine';
 
 import "./assets/css/general.css";
 import "./assets/js/script.js";
@@ -15,7 +16,34 @@ import "./assets/js/script.js";
 class App extends Component {
   state = {
     highScore: 0,
-    currentScore: 0
+    currentScore: 0,
+    gameArea: [],
+    nextShape: {},
+    gameStatus: 0,
+    game: null
+  }
+
+  componentDidMount() {
+    let areaHeight = 20;
+    let areaWidth = 15;
+
+    let renderFunct = gameState => {
+      let newState = {
+        currentScore: this.currentScore + gameState.statistic.countLinesReduced,
+        gameArea: gameState.body,
+        nextShape: gameState.nextShape,
+        gameStatus: gameState.gameStatus
+      };
+      this.setState(newState);
+    }
+
+    const game = new Engine(
+      areaHeight,
+      areaWidth,
+      renderFunct
+    );
+
+    this.setState({game: game});
   }
 
   render() {
