0b6ea2fc6c001b432857080fa9bda758efa00499
rizho123 <richardzhou123@gmail.com>
2019-10-30 00:58:00 -0700

    what

--- client/src/assets/js/script.js
+++ client/src/assets/js/script.js
@@ -3,3 +3,18 @@ import $ from "jquery";
 $(document).on("click", "#leaderboards", function() {
     $(".leaderContainer").fadeToggle(450);
 })
+
+$(document).ready(function() {
+
+var rn = 20;
+var cn = 15;
+  
+ for(var r=0;r<parseInt(rn,10);r++) {
+   var row = $("<tr>")
+   for(var c=0;c<parseInt(cn,10);c++) {
+        var cell = $("<td>")
+        row.append(cell)
+    }
+    $(".game-table").append(row);
+   }
+})
--- client/src/components/GameContainer/index.js
+++ client/src/components/GameContainer/index.js
@@ -3,6 +3,8 @@ import "./style.css";
 
 const GameContainer = props => (
     <div className="GameContainer">
+        <table className="game-table" id="game-table">
+        </table>
     </div>
 );
 
