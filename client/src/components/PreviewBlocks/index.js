d0f28f3e63c90dc1fa968c2e3ab103169074a5e2
rizho123 <v-riczho@microsoft.com>
2019-11-01 05:48:59 -0700

    updating some more styling

--- client/src/components/PreviewBlocks/index.js
+++ client/src/components/PreviewBlocks/index.js
@@ -3,13 +3,13 @@ import "./style.css";
 
 function PreviewBlocks (props) {
 
-    const next = props.NextBlock
+    const next = props.NextBlock;
 
     return (
     <div className="right">
         <div className="PreviewBlocks">
             <h3 className="PBtitle">Next Block:</h3>
-            <p className="nextBlock">{next.name}</p>
+            <p className="nextBlock" >{next.name}</p>
             {console.log("next: ", next)}
         </div>
         <div className="Info">
