5e90106de49fcedae752722ace9a77ddc35b8da5
Kira <kira.mueller@outlook.com>
2019-11-02 12:22:32 -0700

    broke game object creator into helper function

--- client/src/App.js
+++ client/src/App.js
@@ -35,11 +35,12 @@ class App extends Component {
     correctLetters: 0,
     currentShapeName: ""
   }
-  
-  componentDidMount() {
+
+  makeNewGame =() => {
+
     let areaHeight = 20;
     let areaWidth = 25;
-
+  
     let renderFunct = gameState => {
       let score = this.state.currentScore;
       let newScore = gameState.statistic.countLinesReduced;
@@ -49,9 +50,10 @@ class App extends Component {
         nextShape: gameState.nextShape,
         gameStatus: gameState.gameStatus,
         gameSpeed: newScore > score ? this.state.gameSpeed - ((newScore - score) * 10) : this.state.gameSpeed,
+        highScore: newScore > this.state.highScore ? newScore : this.state.highScore,
         currentShapeName: gameState.shapeName
       };
-
+  
       if(this.state.currentShapeName !== gameState.shapeName) {
         this.handleTypeTime();
         newState.gameSpeed--;
@@ -62,16 +64,21 @@ class App extends Component {
       }
       this.setState(newState);
     }
-
+  
     const game = new Engine(
       areaHeight,
       areaWidth,
       renderFunct
     );
 
+    return game;
+  };
+  
+  componentDidMount() {
+
     // console.log('game is: ', game);
 
-    this.setState({ game: game }, () => {
+    this.setState({ game: this.makeNewGame() }, () => {
       // console.log("State changed.")
       // console.log(this.state.game)
     });
