7162aa42c8c6cd1868c8795b271db7d58c8c6e9a
rizho123 <richardzhou123@gmail.com>
2019-11-02 13:16:06 -0700

    updating styles

--- client/src/assets/js/script.js
+++ client/src/assets/js/script.js
@@ -8,23 +8,9 @@ $(document).on("click", ".start", function() {
     $(".start").css("display", "none");
 })
 
-$(document).on("keyup", "body", function(e) {
-    var t = e.key;
-    console.log("K :", t)
-    var text = document.getElementById("currentWord").innerText,
-        pos = text.search(t);
-        console.log("RESULTS: ")
-    console.log(text)
-    console.log(pos)
-    if(pos > -1){
-        $('.currentWord').html(text.substring(0,pos)+'<span class="highlight">'+t+'</span>'+text.substring(pos+t.length));      
-        pos ++;
-        console.log("POS NOW: ", pos)
-    }else{
-        $('.currentWord').text(text);
-    }
-console.log("KEY: ", t);
+$(document).on("click", ".restart", function() {
+  $(".restart").css("display", "none");
+})
 
-});
 
 
