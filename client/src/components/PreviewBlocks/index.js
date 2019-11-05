e58d0210061685a3f981f6d2638172ba810f611c
Kira <kira.mueller@outlook.com>
2019-11-04 19:31:40 -0800

    Add instructions+fix word showing pre game start

--- client/src/components/PreviewBlocks/index.js
+++ client/src/components/PreviewBlocks/index.js
@@ -25,7 +25,11 @@ function PreviewBlocks (props) {
         </div>
         <div className="Info">
             <h1 className = "title">Instructions:</h1>
-            <p className="infoText">Etc...</p>
+            <p className="infoText"><ul>
+                    <li>Control blocks with arrow keys</li>
+                    <li>Controls are locked for each block until you type the word shown</li>
+                    <li>Don't mistype or the game will speed up!</li>
+                </ul></p>
         </div>
     </div>
     );
