4eb1954ccbb592cab65c38cd2b3eaac3038174c6
Jason Allen Richard <Slayermonster0@gmail.com>
2019-11-05 23:32:55 -0800

    no longer saving and duping saves

--- server.js
+++ server.js
@@ -19,24 +19,30 @@ app.get("/api/randomword", (req,res) => {
   res.json({word: RandomWord()});
 });
 
+
 app.post("/signin", (req, res) => {
   // add logic to store user details in DB.
   // req.body.googleId, req.body.highScore, etc...
-  connection.query(`SELECT * FROM users WHERE googleid=${req.body.googleId}`, function (result) {
-    if (!result) {
-      connection.query('INSERT INTO users (name, googleid, highscore) VALUES (?,?,?)', [req.body.name, req.body.googleId, req.body.highScore], function (newUser) {
+  connection.query(`SELECT * FROM users WHERE googleid=${req.body.googleId}`, function (err,result) {
+    if (err) throw err;
+    console.log(JSON.stringify(result));
+    if (!result.length) {
+      connection.query('INSERT INTO users (name, googleid, highscore) VALUES (?,?,?)', [req.body.name, req.body.googleId, req.body.highScore], function (err,newUser) {
+        if(err) throw err;
         console.log('Successfully saved id and name to db.', newUser);
-        res.json({success: true, msg: 'User details saved to DB'}, newUser);
+        res.json({success: true, msg: 'User details saved to DB', user: newUser});
       });
     } else {
-      res.json({success: true, msg: 'User already exists'}, result);
+      console.log("User already exists");
+      res.json({success: true, msg: 'User already exists', result: result});
     }
   });
   
 });
 
 app.post("/updatehighscore/:id", (req, res) => {
-  connection.query('INSERT INTO users (name, googleid, highscore) VALUES (?,?,?)', [req.body.name, req.body.googleId, req.body.highScore], function (result) {
+  connection.query('INSERT INTO users (name, googleid, highscore) VALUES (?,?,?)', [req.body.name, req.body.googleId, req.body.highScore], function (err, result) {
+    if (err) throw err;
     console.log('Successfully saved id and name to db.', result);
     res.json({success: true, msg: 'User details saved to DB'});
   });
